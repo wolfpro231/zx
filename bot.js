25ca84615bf71355a4b997cb6d987aedd7b9f23c
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-05-06 21:18:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '!'
 
 
-const developers = ["ID","ID"]
+const developers = ["536126359966187530"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
